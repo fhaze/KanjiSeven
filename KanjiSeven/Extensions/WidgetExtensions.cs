015c8b625bca44f98b82f515a736233268ebc49e
FHaze <eder_arm@icloud.com>
2018-01-11 02:16:56 -0200

    Alastor commit 9

---
+++ KanjiSeven/Extensions/WidgetExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Gdk;
+using Gtk;
+using Window = Gtk.Window;
+
+namespace KanjiSeven.Extensions
+{
+    public static class WidgetExtensions
+    {
+        public static void DrawResource(this Window window, object obj)
+        {
+            if (obj is Bitmap bmp)
+            {
+                using (var stream = new MemoryStream())
+                {
+                    bmp.Save(stream, ImageFormat.Png);
+                    stream.Position = 0;
+                    window.GdkWindow.DrawPixbuf(new GC(window.GdkWindow), new Pixbuf(stream), 0, 0, 0, 0, bmp.Width,
+                        bmp.Height,
+                        RgbDither.None, 0, 0);   
+
+                }
+            }
+        }
+        
+        public static void DrawResource(this Widget widget, object obj)
+        {
+            if (obj is Bitmap bmp)
+            {
+                using (var stream = new MemoryStream())
+                {
+                    bmp.Save(stream, ImageFormat.Png);
+                    stream.Position = 0;
+                    widget.GdkWindow.DrawPixbuf(new GC(widget.GdkWindow), new Pixbuf(stream), 0, 0, 0, 0, bmp.Width,
+                        bmp.Height,
+                        RgbDither.None, 0, 0);
+                }
+            }
+        }
+    }
+}
--- KanjiSeven/Views/MainForm.cs
+++ KanjiSeven/Views/MainForm.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Gdk;
 using GLib;
 using Gtk;
 using KanjiSeven.Data;
+using KanjiSeven.Extensions;
+using Window = Gtk.Window;
 
 namespace KanjiSeven.Views
 {
     public class MainForm : Window
     {
-        private readonly VBox       _mainVerticalBox = new VBox();
-        private readonly Statusbar  _statusbar       = new Statusbar();
-        private readonly VButtonBox _mainVButtonBox  = new VButtonBox
+        private readonly Table       _table           = new Table(1, 3, true);
+        private readonly VBox        _mainVerticalBox = new VBox();
+        private readonly Gtk.Image   _background      = new Gtk.Image();
+        private readonly Statusbar   _statusbar       = new Statusbar();
+        private readonly VButtonBox  _mainVButtonBox  = new VButtonBox
             { Layout = ButtonBoxStyle.Center, Spacing = 10 };
         
         private readonly Button _startButton  = new Button { Label = "ゲーム"};
@@ -19,15 +26,17 @@ namespace KanjiSeven.Views
         private readonly Button _exitButton   = new Button { Label = "終了"};
         
         public MainForm() : base("Kanji Seven")
-        {            
-            Resize(250, 175);
+        {
+            SetSizeRequest(511, 320);
+            Resizable = false;
             
             _mainVButtonBox.Add(_startButton);
             _mainVButtonBox.Add(_kotobaButton);
             _mainVButtonBox.Add(_configButton);
             _mainVButtonBox.Add(_exitButton);
             
-            _mainVerticalBox.PackStart(_mainVButtonBox);
+            _table.Attach(_mainVButtonBox, 2, 3, 0, 1);
+            _mainVerticalBox.PackStart(_table);
             _mainVerticalBox.PackStart(_statusbar, false, true, 0);
             Add(_mainVerticalBox);
             
@@ -35,9 +44,18 @@ namespace KanjiSeven.Views
             _kotobaButton.Clicked += KanjiButtonOnClicked;
             _configButton.Clicked += ConfigButtonOnClicked;
             _exitButton.Clicked += ExitButtonOnClicked;
+
+            _mainVerticalBox.ExposeEvent += OnExposeEvent;
             
             ShowAll();
             GrabFocus();
+        }
+
+        [ConnectBefore]
+        private void OnExposeEvent(object sender, ExposeEventArgs args)
+        {
+            var vbox = sender as VBox;
+            vbox.DrawResource(Resources.ResourceManager.GetObject("background"));
         }
 
         private void StartButtonOnClicked(object sender, EventArgs eventArgs)
